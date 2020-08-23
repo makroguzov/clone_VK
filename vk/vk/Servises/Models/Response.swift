ee17038a1b0d899641bd527f4bc5879a8cba0adb
makroguzov <valera.nedbai.developer@gmail.com>
2020-08-23 23:02:25 +0300

    контролеры

---
+++ vk/vk/Servises/Models/Response.swift
@@ -0,0 +1,9 @@
+//
+//  Response.swift
+//  vk
+//
+//  Created by MACUSER on 23.08.2020.
+//  Copyright © 2020 MACUSER. All rights reserved.
+//
+
+import Foundation
--- vk/vk/Servises/Models/UserModel.swift
+++ vk/vk/Servises/Models/UserModel.swift
@@ -7,30 +7,51 @@
 //
 
 import Foundation
+import RealmSwift
 
-struct UserModel: Decodable {
-    let id: Int
+class UserModel: Object, Decodable {
+    @objc dynamic var id: Int
 
-    let first_name: String
-    let last_name: String
+    @objc dynamic var first_name: String
+    @objc dynamic var last_name: String
     
-    var can_access_closed: Bool?
-    var is_closed: Bool?
+    let  can_access_closed = RealmOptional<Bool>()
+    let  is_closed = RealmOptional<Bool>()
 
-    var deactivated: String?
+    @objc dynamic var deactivated: String?
 
-    var bdate: String?
-    var city: City?
+    @objc dynamic var bdate: String?
+    @objc dynamic var city: City?
 
-    var photo_100: String?
-    var photo_200: String?
-    var photo_50: String?
-    var photo_200_orig: String?
+    @objc dynamic var photo_100: String?
+    @objc dynamic var photo_200: String?
+    @objc dynamic var photo_50: String?
+    @objc dynamic var photo_200_orig: String?
 }
 
-extension UserModel {
-    struct City: Decodable {
-        let id: Int
-        let title: String
-    }
-}
+//struct UserModel: Decodable {
+//    let id: Int
+//
+//    let first_name: String
+//    let last_name: String
+//
+//    var can_access_closed: Bool?
+//    var is_closed: Bool?
+//
+//    var deactivated: String?
+//
+//    var bdate: String?
+//    var city: City?
+//
+//    var photo_100: String?
+//    var photo_200: String?
+//    var photo_50: String?
+//    var photo_200_orig: String?
+//}
+//
+//extension UserModel {
+//    struct City: Decodable {
+//        let id: Int
+//        let title: String
+//    }
+//}
